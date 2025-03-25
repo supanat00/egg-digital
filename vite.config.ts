0dc0b8ab027cbd1b589d27cf19301a11f573c7be
supanat00 <supanat90@gmail.com>
2025-03-25 14:04:05 +0700

    first-commit

---
+++ vite.config.ts
@@ -0,0 +1,55 @@
+// vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
+
+const manifestForPlugin: Partial<VitePWAOptions> = {
+  registerType: "prompt",
+  includeAssets: ['favicon.ico', "apple-touc-icon.png", "masked-icon.png"],
+  manifest: {
+    name: "Egg Digital",
+    short_name: "Egg Digital",
+    description: "An app that can show the Egg Digital Image Tracking.",
+    icons: [
+      {
+        src: "icons/icons-192.png",
+        sizes: "192x192",
+        type: "image/png"
+      },
+      {
+        src: "icons/icons-256.png",
+        sizes: "256x256",
+        type: "image/png",
+        purpose: "favicon"
+      },
+      {
+        src: "icons/icons-180.png",
+        sizes: "180x180",
+        type: "image/png",
+        purpose: "apple touch icon"
+      },
+      {
+        src: "icons/icons-144.png",
+        sizes: "144x144",
+        type: "image/png",
+        purpose: "any"
+      },
+      {
+        src: "icons/icons-256.png",
+        sizes: "256x256",
+        type: "image/png",
+        purpose: "icon"
+      },
+    ],
+    theme_color: "#181818",
+    background_color: "#e8eac2",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "any",
+  },
+};
+
+export default defineConfig({
+  plugins: [react(), VitePWA(manifestForPlugin)],
+});
