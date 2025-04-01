d86323e61e34fe8eb58b08008ba767d7badcb592
supanat00 <supanat90@gmail.com>
2025-04-02 02:16:13 +0700

    ideal-setting

--- src/components/MindAR.tsx
+++ src/components/MindAR.tsx
@@ -37,8 +37,8 @@ function MindAR() {
         audio: false,
         video: {
           facingMode: 'environment',
-          width: { ideal: 1080 },
-          height: { ideal: 1920 },
+          width: { ideal: 1920 },
+          height: { ideal: 1080 },
         },
       })
 
